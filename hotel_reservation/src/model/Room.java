0da518caeaa7b80774fc0539dfcbd873b0668342
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-22 23:54:09 -0400

    refactor: Refactoring production code
    Add "RoomType" enum declaration variable in "Room.java" java file in "model" package

--- hotel_reservation/src/model/Room.java
+++ hotel_reservation/src/model/Room.java
@@ -1,10 +1,9 @@
 package model;
 
-public class Room {
+public class Room implements IRoom {
     String roomNumber;
     Double price;
-
-
+    public RoomType roomType; // Enum declaration variable
 
     @Override
     public String toString(){ return "Room Number is " + roomNumber + " and Price is $" + price; }

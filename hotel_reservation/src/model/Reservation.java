20dec7cddbb697bf7fd75ddca89a47338c755f9d
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-23 19:49:37 -0400

    refactor: Refactoring production code
    Add "toString" override method in "Reservation.java" java file in "model" package

--- hotel_reservation/src/model/Reservation.java
+++ hotel_reservation/src/model/Reservation.java
@@ -7,4 +7,7 @@ public class Reservation {
     IRoom room;
     Date checkInDate;
     Date checkOutDate;
+
+    @Override
+    public String toString() { return "Customer is " + customer + "\nRoom is " + room + "\nCheck-in date is " + checkInDate + "\nCheck-out date is " + checkOutDate; }
 }

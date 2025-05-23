a8a76a9d293055a7ab2c0f9ad06f32d5af20065a
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-23 01:07:53 -0400

    refactor: Refactoring production code
    Add "toString" override method to "Customer.java" java file in "model" package

--- hotel_reservation/src/model/Customer.java
+++ hotel_reservation/src/model/Customer.java
@@ -2,4 +2,9 @@ package model;
 
 public class Customer {
     String firstName, lastName, email;
+
+    @Override
+    public String toString() {
+        return "Customer Name: " + firstName + " " + lastName + " and Customer Email: " + email;
+    }
 }

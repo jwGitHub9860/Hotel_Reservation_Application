afe651df0092900cb45d8c17ef5a366c7e6b8ac7
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-22 22:44:51 -0400

    refactor: Refactoring production code
    Add "roomNumber" and "price" variables and "toString()" override method to "Room.java" java file in "model" package

--- hotel_reservation/src/model/Room.java
+++ hotel_reservation/src/model/Room.java
@@ -1,6 +1,11 @@
 package model;
 
 public class Room {
+    String roomNumber;
+    Double price;
     // Room Types
     enum RoomType{ SINGLE, DOUBLE }
+
+    @Override
+    public String toString(){ return "Room Number is " + roomNumber + " and Price is $" + price; }
 }

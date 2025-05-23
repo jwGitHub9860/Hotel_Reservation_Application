bac20bd2a1c23c53497ace312b280a763bff062c
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-23 00:31:19 -0400

    refactor: Refactoring production code
    Add "toString" override method to "FreeRoom.java" java file in "model" package

--- hotel_reservation/src/model/FreeRoom.java
+++ hotel_reservation/src/model/FreeRoom.java
@@ -2,5 +2,8 @@ package model;
 
 public class FreeRoom extends Room {
     // Constructor
-    public FreeRoom(Double price){ price = 0.0; }
+    public FreeRoom(Double price) { price = 0.0; }
+
+    @Override
+    public String toString() { return "The room is free!"; }
 }

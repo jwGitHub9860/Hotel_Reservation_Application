44d0316e56bb7dca75add696c247284ba5398241
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-24 14:55:02 -0400

    refactor: Refactoring production code
    Add nine print statements to create menu items in main method of "Main.java" java file

--- hotel_reservation/src/Main.java
+++ hotel_reservation/src/Main.java
@@ -8,5 +8,15 @@ public class Main {
 
         System.out.printf("\n\nTest 2:\n"); // TESTING CODE
         Customer customer2 = new Customer("first", "second", "email"); // TESTING CODE
+
+        System.out.println("Welcome to the Hotel Reservation Application");
+        System.out.println("-------------------------------------------------");
+        System.out.println("1. Find and reserve a room");
+        System.out.println("2. See my reservations");
+        System.out.println("3. Create an Account");
+        System.out.println("4. Admin");
+        System.out.println("5. Exit");
+        System.out.println("-------------------------------------------------");
+        System.out.println("Please select a number for the menu option\n");
     }
 }

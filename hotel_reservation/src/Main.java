efbb8e2704ddd1c6b891f1f3ecbee7bf07a78919
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-24 15:49:29 -0400

    test: Adding tests, refactoring test
    Comment out all testing code above code for main menu in main method of "Main.java" java file

--- hotel_reservation/src/Main.java
+++ hotel_reservation/src/Main.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Customer customer = new Customer("first", "second", "j@domain.com"); // TESTING CODE
+        /*Customer customer = new Customer("first", "second", "j@domain.com"); // TESTING CODE
 
         System.out.printf(String.valueOf(customer)); // TESTING CODE
 
         System.out.printf("\n\nTest 2:\n"); // TESTING CODE
-        Customer customer2 = new Customer("first", "second", "email"); // TESTING CODE
+        Customer customer2 = new Customer("first", "second", "email");*/ // TESTING CODE
 
         // Main Menu
         boolean runApplication = true;

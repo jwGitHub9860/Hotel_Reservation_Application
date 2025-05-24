24411d4dcfbee992b6a15810f8468cf1f72caf9a
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-24 13:27:32 -0400

    refactor: Refactoring production code
    Add "getCustomer", "addRoom", "getAllRooms", "getAllCustomers", and "displayAllReservations" methods in "AdminResource.java" java file in "api" package

--- hotel_reservation/src/api/AdminResource.java
+++ hotel_reservation/src/api/AdminResource.java
@@ -1,4 +1,19 @@
 package api;
 
+import model.Customer;
+import model.IRoom;
+
+import java.util.Collection;
+import java.util.List;
+
 public class AdminResource {
+    public Customer getCustomer(String email) { return; }
+
+    public void addRoom(List<IRoom> rooms) {}
+
+    public Collection<IRoom> getAllRooms() { return; }
+
+    public Collection<Customer> getAllCustomers() { return; }
+
+    public void displayAllReservations() {}
 }

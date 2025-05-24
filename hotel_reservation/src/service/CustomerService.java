6c5c2d4a372847d44d71966030fb1a282c2bc92f
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-23 23:58:54 -0400

    refactor: Refactoring production code
    Add "addCustomer", "getCustomer", and "Collection" methods to "CustomerService.java" java file in "service" package

--- hotel_reservation/src/service/CustomerService.java
+++ hotel_reservation/src/service/CustomerService.java
@@ -1,4 +1,15 @@
 package service;
 
+import model.Customer;
+
+import java.util.Collection;
+
 public class CustomerService {
+    public void addCustomer(String email, String firstName, String lastName) {
+        email = "i"; // ADD CORRECT CODE
+    }
+
+    public Customer getCustomer(String customerEmail) { return; } // ADD CORRECT CODE
+
+    public Collection<Customer> getAllCustomers() { return; } // ADD CORRECT CODE
 }

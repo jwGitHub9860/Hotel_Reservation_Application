78344e1916941bba7cde6c0a37080e208d94936f
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-24 15:57:41 -0400

    test: Adding tests, refactoring test
    Comment out "getARoom", "reserveARoom", "findRooms", and "getCustomersReservation" methods in "ReservationService" class of "ReservationService.java" java file in "service" package

--- hotel_reservation/src/service/ReservationService.java
+++ hotel_reservation/src/service/ReservationService.java
@@ -18,13 +18,13 @@ public class ReservationService {
 
     public void addRoom(IRoom room) {}
 
-    public IRoom getARoom(String roomId) { return; }
+    /*public IRoom getARoom(String roomId) { return; }
 
     public Reservation reserveARoom(Customer customer, IRoom room, Date checkInDate, Date checkOutDate) { return; }
 
     public Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate) { return; }
 
-    public Collection<Reservation> getCustomersReservation(Customer customer) { return; }
+    public Collection<Reservation> getCustomersReservation(Customer customer) { return; }*/
 
     public void printAllReservation() {}
 }

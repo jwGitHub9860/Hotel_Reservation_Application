2c75455b7656387f2b7e23f72bb42f32841acab4
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-23 20:02:58 -0400

    test: Adding tests, refactoring test
    Replace first two comments in main method with testing code to test if "Customer.java" java file works in "Main.java" java file

--- hotel_reservation/src/Main.java
+++ hotel_reservation/src/Main.java
@@ -1,7 +1,8 @@
+import model.Customer;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        Customer customer = new Customer("first", "second", "j@domain.com"); // TESTING CODE
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {

859cad2ea113d5c78600cde3e348d79c5d38dd06
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-23 20:37:37 -0400

    fix: A bug fix
    Add method inputs to all room methods in "IRoom.java" interface java file to match room methods in "Room.java" java file in "model" package

--- hotel_reservation/src/model/IRoom.java
+++ hotel_reservation/src/model/IRoom.java
@@ -2,8 +2,8 @@ package model;
 
 // Room Methods
 public interface IRoom {
-    public String getRoomNumber();
-    public Double getRoomPrice();
-    public RoomType getRoomType();
+    public String getRoomNumber(String roomNumber);
+    public Double getRoomPrice(Double price);
+    public RoomType getRoomType(RoomType roomType);
     public boolean isFree();
 }

f4ef6cbbc55242124d1b61dc5af165b72790a44a
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-23 20:43:20 -0400

    test: Adding tests, refactoring test
    Add three testing print statements in "Customer.java" java file in "model" package to find out how error takes place and where error occurs

--- hotel_reservation/src/model/Customer.java
+++ hotel_reservation/src/model/Customer.java
@@ -9,11 +9,14 @@ public class Customer {
     public Customer(String firstName, String lastName, String email) { // Checks if email format is correct
         // Checks if there's only 1 @ in email
         int atSymbolAmount = 0; // amount of @ in email
+        System.out.println("In Customer constructor"); // TESTING CODE
         for (int i = 0; i < email.length(); i++) { // finds amount of @ in "email"
+            System.out.println("atSymbolAmount amount: " + atSymbolAmount); // TESTING CODE
             if (Objects.equals(email.charAt(i), "@")){
                 atSymbolAmount++;
             }
         }
+        System.out.println("atSymbolAmount amount: " + atSymbolAmount); // TESTING CODE
         if (atSymbolAmount != 1) { throw new IllegalArgumentException("Email must have 1 \"@\""); }
 
         // Checks if last 4 characters are ".com"

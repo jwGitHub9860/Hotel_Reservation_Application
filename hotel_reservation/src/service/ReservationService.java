0f17aadd731dfb8d962fb3d4817cafd4c4aff574
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-24 00:09:43 -0400

    refactor: Refactoring production code
    Add "addRoom", "getARoom", "reserveARoom", "findRooms", "getCustomersReservation", and "printAllReservation" methods in "ReservationService.java" java file in "service" package

--- hotel_reservation/src/service/ReservationService.java
+++ hotel_reservation/src/service/ReservationService.java
@@ -1,4 +1,22 @@
 package service;
 
+import model.Customer;
+import model.IRoom;
+import model.Reservation;
+
+import java.util.Collection;
+import java.util.Date;
+
 public class ReservationService {
+    public void addRoom(IRoom room) {}
+
+    public IRoom getARoom(String roomId) { return; }
+
+    public Reservation reserveARoom(Customer customer, IRoom room, Date checkInDate, Date checkOutDate) { return; }
+
+    public Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate) { return; }
+
+    public Collection<Reservation> getCustomersReservation(Customer customer) { return; }
+
+    public void printAllReservation() {}
 }

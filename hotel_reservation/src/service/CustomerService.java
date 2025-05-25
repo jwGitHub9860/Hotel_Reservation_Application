c1913939fc7c9f4a9bbc6ecfa8a18a305f574259
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-25 17:14:22 -0400

    test: Adding tests, refactoring test
    Uncomment second comment out print statement in "addCustomer" method of "CustomerService.java" java file in "service" package to test if "addCustomer" works

--- hotel_reservation/src/service/CustomerService.java
+++ hotel_reservation/src/service/CustomerService.java
@@ -9,8 +9,8 @@ public class CustomerService {
         Customer customer = new Customer(firstName, lastName, email); // calls "Customer" constructor
 
         //System.out.printf(String.valueOf(customer)); // TESTING CODE
-        /*System.out.printf("\n\nTest 2:\n"); // TESTING CODE
-        Customer customer2 = new Customer("first", "second", "email");*/ // TESTING CODE
+        System.out.printf("\n\nTest 2:\n"); // TESTING CODE
+        /*Customer customer2 = new Customer("first", "second", "email");*/ // TESTING CODE
     }
 
     /*public Customer getCustomer(String customerEmail) { return; } // ADD CORRECT CODE

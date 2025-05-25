257641192e98cce76ca613c782e8f8657f31a675
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-25 01:36:57 -0400

    refactor: Refactoring production code
    Delete last code line in "addCustomer" method in "CustomerService" class of "CustomerService.java" java class in "service" package

--- hotel_reservation/src/service/CustomerService.java
+++ hotel_reservation/src/service/CustomerService.java
@@ -9,10 +9,8 @@ public class CustomerService {
         Customer customer = new Customer("first", "second", "j@domain.com"); // TESTING CODE
 
         /*System.out.printf(String.valueOf(customer)); // TESTING CODE
-
         System.out.printf("\n\nTest 2:\n"); // TESTING CODE
         Customer customer2 = new Customer("first", "second", "email");*/ // TESTING CODE
-        email = "i"; // ADD CORRECT CODE
     }
 
     /*public Customer getCustomer(String customerEmail) { return; } // ADD CORRECT CODE

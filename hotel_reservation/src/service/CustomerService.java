545c7e576ea4de865fd3890bc9c73cd91be194c2
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-25 16:29:27 -0400

    test: Adding tests, refactoring test
    Comment out uncomment testing print statement in "addCustomer" method of "CustomerService.java" java file

--- hotel_reservation/src/service/CustomerService.java
+++ hotel_reservation/src/service/CustomerService.java
@@ -8,7 +8,7 @@ public class CustomerService {
     public static void addCustomer(String email, String firstName, String lastName) {
         Customer customer = new Customer(firstName, lastName, email); // calls "Customer" constructor
 
-        System.out.printf(String.valueOf(customer)); // TESTING CODE
+        //System.out.printf(String.valueOf(customer)); // TESTING CODE
         /*System.out.printf("\n\nTest 2:\n"); // TESTING CODE
         Customer customer2 = new Customer("first", "second", "email");*/ // TESTING CODE
     }

2e0ef3cf5b26cd7f977d97aa69eb8925a0894064
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-26 18:28:22 -0400

    refactor: Refactoring production code
    Change "displayAllReservations" and "addRoom" methods into "static" methods in "AdminResource.java" java file in "api" package

--- hotel_reservation/src/api/AdminResource.java
+++ hotel_reservation/src/api/AdminResource.java
@@ -10,11 +10,11 @@ import java.util.List;
 public class AdminResource {
     //public Customer getCustomer(String email) { return; }
 
-    public void addRoom(List<IRoom> rooms) {}
+    public static void addRoom(List<IRoom> rooms) {}
 
     /*public Collection<IRoom> getAllRooms() { return; }
 
     public Collection<Customer> getAllCustomers() { return; }*/
 
-    public void displayAllReservations() { ReservationService.printAllReservation(); } // calls "printAllReservation()" method from "ReservationService.java" SERVICE file
+    public static void displayAllReservations() { ReservationService.printAllReservation(); } // calls "printAllReservation()" method from "ReservationService.java" SERVICE file
 }

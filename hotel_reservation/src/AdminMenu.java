e254e17f6fb127905e46ed87a90a1471bf26ae1f
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-26 01:44:18 -0400

    refactor: Refactoring production code
    Add "while loop" around code for taking user input for room information in "if-else statement" with "adminUserInput == 4" condition in "AdminMenu.java" java file

--- hotel_reservation/src/AdminMenu.java
+++ hotel_reservation/src/AdminMenu.java
@@ -41,15 +41,17 @@ public class AdminMenu {
                         ReservationService.printAllReservation(); // calls "printAllReservation()" method
                     } else if (adminUserInput == 4) {
                         String addRoomRepeat = "y"; // initial "addRoomRepeat" value
-                        // Takes User Input for Room Information
-                        System.out.println("Enter room number: ");
-                        String roomNumber = scanner.nextLine(); // takes User Input for "roomNumber"
-                        System.out.println("Enter price per night: ");
-                        Double roomPrice = scanner.nextDouble(); // takes User Input for "roomPrice"
-                        System.out.println("Enter room type (1 for single bed, 2 for double bed): ");
-                        int roomType = scanner.nextInt(); // takes User Input for "roomType"
-                        System.out.println("Would you like to add another room (y/n): ");
-                        addRoomRepeat = scanner.next(); // takes User Input to confirm if user wants to add another room
+                        while (addRoomRepeat.equals("y")) {
+                            // Takes User Input for Room Information
+                            System.out.println("Enter room number: ");
+                            String roomNumber = scanner.nextLine(); // takes User Input for "roomNumber"
+                            System.out.println("Enter price per night: ");
+                            Double roomPrice = scanner.nextDouble(); // takes User Input for "roomPrice"
+                            System.out.println("Enter room type (1 for single bed, 2 for double bed): ");
+                            int roomType = scanner.nextInt(); // takes User Input for "roomType"
+                            System.out.println("Would you like to add another room (y/n): ");
+                            addRoomRepeat = scanner.next(); // takes User Input to confirm if user wants to add another room
+                        }
                     } else if (adminUserInput == 5) {
                         System.out.println("ADMINadminUserInput 5 WORKS"); // TESTING CODE
                     } else if (adminUserInput == 6) {

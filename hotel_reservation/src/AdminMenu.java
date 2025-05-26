04870e318cdaf1866014695d0c27e49f3ae92f76
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-26 13:56:18 -0400

    refactor: Refactoring production code
    Cut all code in "if-else statement" with "adminUserInput == 4" condition in "Admin" class of "AdminMenu.java" java file and paste it into "addRoom" method in "RerservationService" class of "RerservationService.java" java file in "service" package

--- hotel_reservation/src/AdminMenu.java
+++ hotel_reservation/src/AdminMenu.java
@@ -42,53 +42,7 @@ public class AdminMenu {
                     } else if (adminUserInput == 3) {
                         ReservationService.printAllReservation(); // calls "printAllReservation()" method
                     } else if (adminUserInput == 4) {
-                        String addRoomRepeat = "y"; // initial "addRoomRepeat" value
-                        while (addRoomRepeat.equals("y")) {
-                            // Takes User Input for Room Information
-                            System.out.println("Enter room number: ");
-                            String roomNumber = scanner.nextLine(); // takes User Input for "roomNumber"
 
-                            // ensures that user inputted "roomNumber"
-                            if (roomNumber.isEmpty()) { roomNumber = scanner.nextLine(); }
-
-                            System.out.println("Enter price per night: ");
-                            Double roomPrice = scanner.nextDouble(); // takes User Input for "roomPrice"
-
-                            // Checks if user inputted "1" or "2"
-                            System.out.println("Enter room type (1 for single bed, 2 for double bed): ");
-                            while (true) {
-                                int roomTypeInput = scanner.nextInt(); // takes User Input for "roomTypeInput"
-
-                                switch (roomTypeInput) {
-                                    case 1:
-                                        Room.roomType = RoomType.SINGLE;
-                                        break;
-                                    case 2:
-                                        Room.roomType = RoomType.DOUBLE;
-                                        break;
-                                    default:
-                                        System.out.println("Please enter 1 or 2: ");
-                                        break;
-                                }
-
-                                if (roomTypeInput == 1 || roomTypeInput == 2) { break; } // exits while loop if "roomTypeInput" is "1" or "2"
-                            }
-
-                            // Checks if user inputted "y" or "n"
-                            System.out.println("Would you like to add another room (y/n): ");
-                            while (true) {
-                                try {
-                                    addRoomRepeat = scanner.next(); // takes User Input to confirm if user wants to add another room
-                                    if (addRoomRepeat.equals("y") || addRoomRepeat.equals("n")) { // Valid answers
-                                        break;
-                                    } else { // INVALID answer
-                                        throw new IllegalArgumentException("Answer must be \"y\" or \"n\"");
-                                    }
-                                } catch (Exception e) {
-                                    System.out.println("Please enter y (yes) or n (n): ");
-                                }
-                            }
-                        }
                     } else if (adminUserInput == 5) {
                         System.out.println("ADMINadminUserInput 5 WORKS"); // TESTING CODE
                     } else if (adminUserInput == 6) {

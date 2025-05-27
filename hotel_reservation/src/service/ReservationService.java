7f8c827da63c54e3fd05cc57611ba63b1b342e12
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-27 02:35:38 -0400

    test: Adding tests, refactoring test
    Uncomment "getCustomersReservation" method in "ReservationService.java" java file in "service" package

--- hotel_reservation/src/service/ReservationService.java
+++ hotel_reservation/src/service/ReservationService.java
@@ -65,9 +65,9 @@ public class ReservationService {
 
     public Reservation reserveARoom(Customer customer, IRoom room, Date checkInDate, Date checkOutDate) { return; }
 
-    public Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate) { return; }
+    public Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate) { return; }*/
 
-    public Collection<Reservation> getCustomersReservation(Customer customer) { return; }*/
+    public Collection<Reservation> getCustomersReservation(Customer customer) { return; }
 
     public static void printAllReservation() {
         System.out.println("\nADMINadminUserInput 3 WORKS"); // TESTING CODE

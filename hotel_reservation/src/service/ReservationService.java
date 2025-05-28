065fb024abea438b0f04f1826965f29da2375d8d
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-28 16:53:57 -0400

    test: Adding tests, refactoring test
    Uncomment "reserveARoom()" method in "ReservationService.java" java file in "service" package

--- hotel_reservation/src/service/ReservationService.java
+++ hotel_reservation/src/service/ReservationService.java
@@ -84,7 +84,7 @@ public class ReservationService {
 
     public static IRoom getARoom(String roomId) { return roomInstance; } // ADD CORRECT CODE
 
-    //public static Reservation reserveARoom(Customer customer, IRoom room, Date checkInDate, Date checkOutDate) { return; }
+    public static Reservation reserveARoom(Customer customer, IRoom room, Date checkInDate, Date checkOutDate) { return; }
 
     public static Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate) { return roomCollection; }
 

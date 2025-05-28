85cbec1eff1079c026e90698294da626013212fe
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-28 15:08:05 -0400

    refactor: Refactoring production code
    Add code to make "getARoom()" method call equal "IRoom chosenRoomNumber" in main method in "Main.java" java file

--- hotel_reservation/src/Main.java
+++ hotel_reservation/src/Main.java
@@ -116,7 +116,7 @@ public class Main {
                             System.out.printf("\nChoose room number to reserve: ");
                             String chosenRoom = scanner.nextLine(); // takes User Input for "chosenRoom"
 
-                            HotelResource.getRoom(chosenRoom); // calls "getRoom()" method to find "chosenRoom" in "roomCollection"
+                            IRoom chosenRoomNumber = HotelResource.getRoom(chosenRoom); // calls "getRoom()" method to find "chosenRoom" in "roomCollection"
 
                             HotelResource.bookARoom(email, room, checkInDate, checkOutDate); // calls "bookARoom()" method
                         }

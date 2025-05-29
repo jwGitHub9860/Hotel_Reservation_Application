5e83b4b2b8304018a07f2857a08782e0dd686b7f
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-29 15:23:20 -0400

    refactor: Refactoring production code
    Change input of "addRoom()" method call from "ReservationService.java" from "(IRoom) ReservationService.roomCollection" to "(IRoom) rooms" in "addRoom()" method inside "AdminResource.java"
    java file in "api" service

--- hotel_reservation/src/api/AdminResource.java
+++ hotel_reservation/src/api/AdminResource.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class AdminResource {
     //public static Customer getCustomer(String email) { return; }
 
-    public static void addRoom(List<IRoom> rooms) { ReservationService.addRoom((IRoom) ReservationService.roomCollection); } // calls "addRoom()" method from SERVICE file
+    public static void addRoom(List<IRoom> rooms) { ReservationService.addRoom((IRoom) rooms); } // calls "addRoom()" method from SERVICE file
 
     public static Collection<IRoom> getAllRooms() { return ReservationService.roomCollection; }
 

780c509d3f5b51a5c4046c91ab78ea1441193d58
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-29 16:53:06 -0400

    refactor: Refactoring production code
    Cut code inside "addRoom()" method from "ReservationService.java" and paste it inside "if-else statement" with "adminUserInput == 4" condition in "AdminMenu.java" java file in "service" package

--- hotel_reservation/src/AdminMenu.java
+++ hotel_reservation/src/AdminMenu.java
@@ -6,6 +6,7 @@ import model.RoomType;
 import service.CustomerService;
 import service.ReservationService;
 
+import java.text.DecimalFormat;
 import java.util.Collection;
 import java.util.List;
 import java.util.Scanner;
@@ -52,8 +53,69 @@ public class AdminMenu {
                     } else if (adminUserInput == 3) {
                         AdminResource.displayAllReservations(); // calls "displayAllReservations()" method from "AdminResource.java"
                     } else if (adminUserInput == 4) {
+                        // Allows access to "IRoom" interface
+                        Room roomInstance = new Room(); // creates "Room" instance
+
+                        String addRoomRepeat = "y"; // initial "addRoomRepeat" value
+                        while (addRoomRepeat.equals("y")) {
+                            // Takes User Input for Room Information
+                            System.out.println("Enter room number: ");
+                            Room.roomNumber = scanner.nextLine(); // takes User Input for "roomNumber"
+
+                            // Ensures that user inputted "roomNumber"
+                            if (Room.roomNumber.isEmpty()) { Room.roomNumber = scanner.nextLine(); }
+
+                            // Checks if user inputted "Double" value for Room "price"
+                            System.out.println("Enter price per night: ");
+                            while (true) {
+                                try {
+                                    // Ensures that Room "price" is in "#.##" format
+                                    DecimalFormat decimalFormat = new DecimalFormat("#.##"); // creates "DecimalFormat" instance with "#.##" format
+                                    Room.price = Double.valueOf(decimalFormat.format(scanner.nextDouble())); // takes User Input for Room "price" & rounds Room "price" off to "#.##" format
+                                    break;
+                                } catch (Exception e) {
+                                    System.out.println("Please enter price in \"#.##\" format: ");
+                                }
+                            }
+
+                            // Checks if user inputted "1" or "2"
+                            System.out.println("Enter room type (1 for single bed, 2 for double bed): ");
+                            while (true) {
+                                int roomTypeInput = scanner.nextInt(); // takes User Input for "roomTypeInput"
+
+                                switch (roomTypeInput) {
+                                    case 1:
+                                        Room.roomType = RoomType.SINGLE;
+                                        break;
+                                    case 2:
+                                        Room.roomType = RoomType.DOUBLE;
+                                        break;
+                                    default:
+                                        System.out.println("Please enter 1 or 2: ");
+                                        break;
+                                }
+
+                                if (roomTypeInput == 1 || roomTypeInput == 2) { break; } // exits while loop if "roomTypeInput" is "1" or "2"
+                            }
+
+                            // Checks if user inputted "y" or "n"
+                            System.out.println("Would you like to add another room (y/n): ");
+                            while (true) {
+                                try {
+                                    addRoomRepeat = scanner.next(); // takes User Input to confirm if user wants to add another room
+                                    if (addRoomRepeat.equals("y") || addRoomRepeat.equals("n")) { // Valid answers
+                                        break;
+                                    } else { // INVALID answer
+                                        throw new IllegalArgumentException("Answer must be \"y\" or \"n\"");
+                                    }
+                                } catch (Exception e) {
+                                    System.out.println("Please enter y (yes) or n (n): ");
+                                }
+                            }
+                            ReservationService.roomCollection.add(roomInstance); // adds "roomNumber", "price", and "roomType" to "roomCollection"
+                        }
                         // FIGURE OUT HOW TO ADD TO ROOM LIST
-                        AdminResource.addRoom((List<IRoom>) ReservationService.roomCollection); // calls "addRoom()" method from "AdminResource.java"
+                        //AdminResource.addRoom((List<IRoom>) ReservationService.roomCollection); // calls "addRoom()" method from "AdminResource.java"
                     } else if (adminUserInput == 5) {
                         System.out.println("ADMINadminUserInput 5 WORKS"); // TESTING CODE
                     } else if (adminUserInput == 6) {

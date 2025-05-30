04d002a9dfaa6a39384a2c4d5703f597f682c0db
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-29 20:15:34 -0400

    refactor: Refactoring production code
    Add code to call "Room" constructor in "addRoom()" method in "AdminResource.java" java file in "api" package

--- hotel_reservation/src/api/AdminResource.java
+++ hotel_reservation/src/api/AdminResource.java
@@ -2,6 +2,7 @@ package api;
 
 import model.Customer;
 import model.IRoom;
+import model.Room;
 import service.CustomerService;
 import service.ReservationService;
 
@@ -11,7 +12,11 @@ import java.util.List;
 public class AdminResource {
     //public static Customer getCustomer(String email) { return; }
 
-    public static void addRoom(List<IRoom> rooms) { ReservationService.addRoom((IRoom) rooms); } // calls "addRoom()" method from SERVICE file
+    public static void addRoom(List<IRoom> rooms) {
+        // Calls "Room" constructor
+        IRoom room = new Room(); // allows access to "IRoom" interface
+        ReservationService.addRoom((IRoom) rooms); // calls "addRoom()" method from SERVICE file
+    }
 
     public static Collection<IRoom> getAllRooms() { return ReservationService.roomCollection; }
 

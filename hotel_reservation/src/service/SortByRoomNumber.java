36319451c521734f3ac4f15d2d930213334fd489
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-05-31 14:45:07 -0400

    refactor: Refactoring production code
    Add code to sort collection by room number in "SortByRoomNumber" class in "SortByRoomNumber.java" java class in "service" package

--- hotel_reservation/src/service/SortByRoomNumber.java
+++ hotel_reservation/src/service/SortByRoomNumber.java
@@ -5,4 +5,11 @@ import model.Room;
 import java.util.Comparator;
 
 public abstract class SortByRoomNumber implements Comparator<Room> {
+    // Sorts Collection in Ascending Order of Room Numbers
+    @Override
+    public int compare(Room o1, Room o2) {
+        int roomNumber1 = Integer.parseInt(o1.getRoomNumber()); // changes 1st "roomNumber" from "String" to "int"
+        int roomNumber2 = Integer.parseInt(o2.getRoomNumber()); // changes 2nd "roomNumber" from "String" to "int"
+        return Integer.compare(roomNumber1, roomNumber2); // returns "roomNumber1" and "roomNumber2" in Ascending Order
+    }
 }

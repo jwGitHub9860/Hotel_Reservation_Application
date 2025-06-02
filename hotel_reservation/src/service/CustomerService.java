3ff194e78c6a8bc30bac8bf5d07c5248c014a850
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-06-02 00:41:54 -0400

    refactor: Refactoring production code
    Add code to sort "customerCollection" in "addCustomer()" method in "CustomerService.java" java file in "service" package

--- hotel_reservation/src/service/CustomerService.java
+++ hotel_reservation/src/service/CustomerService.java
@@ -3,12 +3,22 @@ package service;
 import model.Customer;
 
 import java.util.Collection;
+import java.util.Comparator;
 
 public class CustomerService {
     public static void addCustomer(String email, String firstName, String lastName) {
         Customer customer = new Customer(firstName, lastName, email); // calls "Customer" constructor
 
         ReservationService.customerCollection.add(customer); // add "customer" to "customerCollection"
+
+        // Sorts "roomList" by Room Numbers
+        ReservationService.customerCollection.sort(Comparator.comparing(person -> {
+            String string = person.getRoomNumber(); // obtains "roomNumber"
+            String[] variables = string.split("\\."); // "\\." - match the character
+            int firstVariable = Integer.parseInt(variables[0]); // obtains 1st Room Number
+            int secondVariable = variables.length > 1 ? Integer.parseInt(variables[1]) : 0; // finds which Room Number is greater
+            return firstVariable * 1000 + secondVariable; // returns "roomNumber1" and "roomNumber2" in Ascending Order
+        }));
     }
 
     public static Customer getCustomer(String customerEmail) {

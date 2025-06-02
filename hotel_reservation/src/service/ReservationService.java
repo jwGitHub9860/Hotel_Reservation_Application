0f6503a68fea187dde3331ad8e9c0a6d348c674d
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-06-02 00:49:13 -0400

    refactor: Refactoring production code
    Change "customerCollection" from "Collection<Customer>" to "List<Customer>" in "ReservationService" class in "ReservationService.java" java file in "service" package to allow "customerCollection" to use ".sort()"

--- hotel_reservation/src/service/ReservationService.java
+++ hotel_reservation/src/service/ReservationService.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class ReservationService {
     // Collections to store & retrieve reservations
-    public static Collection<Customer> customerCollection = new LinkedList<Customer>();
+    public static List<Customer> customerCollection = new LinkedList<Customer>();
 
     // Room Collections
     public static List<IRoom> roomCollection = new ArrayList<>();

ca974a7d15e7e92012266a3926ccf61de5584854
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-06-03 16:57:40 -0400

    refactor: Refactoring production code
    Change "toString()" override method in "Reservation.java" java file in "model" package to display room information of customer reservation more clearly

--- hotel_reservation/src/model/Reservation.java
+++ hotel_reservation/src/model/Reservation.java
@@ -17,5 +17,5 @@ public class Reservation {
     }
 
     @Override
-    public String toString() { return "Reservation:\nCustomer: " + customer.getFirstName() + " " + customer.getLastName() + "\nRoom is " + room + "\nCheck-In Date: " + checkInDate + "\nCheck-Out Date: " + checkOutDate; }
+    public String toString() { return "Reservation:\nCustomer: " + customer.getFirstName() + " " + customer.getLastName() + "\nRoom: " + room.getRoomNumber() + " - " + room.getRoomType() + " bed\nPrice: " + room.getRoomPrice() + " price per night\nCheck-In Date: " + checkInDate + "\nCheck-Out Date: " + checkOutDate; }
 }

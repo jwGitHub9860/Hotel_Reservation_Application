ff4700bc9ded651eb5c79427875cd6dc795651c4
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-06-03 17:15:49 -0400

    refactor: Refactoring production code
    Reorganize comment in "addCustomer()" method in "CustomerService.java" java file in "service" package to reorganize code

--- hotel_reservation/src/service/CustomerService.java
+++ hotel_reservation/src/service/CustomerService.java
@@ -7,9 +7,11 @@ import java.util.Comparator;
 
 public class CustomerService {
     public static void addCustomer(String email, String firstName, String lastName) {
-        Customer customer = new Customer(firstName, lastName, email); // calls "Customer" constructor
+        // Calls "Customer" constructor to obtain WHOLE Customer Information
+        Customer customer = new Customer(firstName, lastName, email);
 
-        ReservationService.customerCollection.add(customer); // add "customer" to "customerCollection"
+        // Adds "customer" to "customerCollection"
+        ReservationService.customerCollection.add(customer);
 
         // Sorts "customerCollection" by First Names in Alphabetical Order
         ReservationService.customerCollection.sort((person1, person2) -> person1.getFirstName().compareTo(person2.getFirstName())); // finds which Room Number is greater

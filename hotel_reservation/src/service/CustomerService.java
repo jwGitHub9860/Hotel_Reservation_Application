12905e2c8ed24b7f5503bc62293b6f308a1534e8
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-06-03 15:40:16 -0400

    test: Adding tests, refactoring test
    Add two testing "for loops" with "print statements" to display "customerCollection" before and after user adds new "customer" to "customerCollection" in "addCustomer()" method in "CustomerService.java" java file in "service" package to see why information of only last "customer" that user adds shows in place of other "customer" information on console when user displays "customerCollection"

--- hotel_reservation/src/service/CustomerService.java
+++ hotel_reservation/src/service/CustomerService.java
@@ -8,9 +8,15 @@ import java.util.Comparator;
 public class CustomerService {
     public static void addCustomer(String email, String firstName, String lastName) {
         Customer customer = new Customer(firstName, lastName, email); // calls "Customer" constructor
-
+        System.out.println("BEFORE:\n"); // TESTING CODE
+        for (Customer customer1 : ReservationService.customerCollection) { // TESTING CODE
+            System.out.println(customer1); // TESTING CODE
+        } // TESTING CODE
         ReservationService.customerCollection.add(customer); // add "customer" to "customerCollection"
-
+        System.out.println("AFTER:\n"); // TESTING CODE
+        for (Customer customer1 : ReservationService.customerCollection) { // TESTING CODE
+            System.out.println(customer1); // TESTING CODE
+        } // TESTING CODE
         // Sorts "customerCollection" by First Names in Alphabetical Order
         ReservationService.customerCollection.sort(Comparator.comparing(person -> Customer.getFirstName())); // finds which Room Number is greater
     }

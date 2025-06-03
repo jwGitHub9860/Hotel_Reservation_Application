3b9d0f5456b3b61a494e2dff954759ee40aed585
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-06-03 03:45:30 -0400

    refactor: Refactoring production code
    Add "Reservation" constructor to "Reservation.java" java file in "model" package to create "Reservation"

--- hotel_reservation/src/model/Reservation.java
+++ hotel_reservation/src/model/Reservation.java
@@ -8,6 +8,14 @@ public class Reservation {
     Date checkInDate;
     Date checkOutDate;
 
+    // Constructor
+    public Reservation(Customer customer, IRoom room, Date checkInDate, Date checkOutDate) {
+        this.customer = customer;
+        this.room = room;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
+
     @Override
     public String toString() { return "Customer is " + customer + "\nRoom is " + room + "\nCheck-in date is " + checkInDate + "\nCheck-out date is " + checkOutDate; }
 }

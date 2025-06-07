bbfe1bad30787f1797027589e5d692f24b5801bb
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-06-07 15:09:35 -0400

    fix: A bug fix
    Replace "import static model.Customer.inputCheckInAndCheckOutDates" with "import static model.Reservation.inputCheckInAndCheckOutDates" inside "Main.java" to fix error of "Main.java" not having access to "inputCheckInAndCheckOutDates()" method

--- hotel_reservation/src/Main.java
+++ hotel_reservation/src/Main.java
@@ -15,7 +15,7 @@ import java.util.Date;
 import java.util.Scanner;
 
 import static model.Customer.inputAccountInformation;
-import static model.Customer.inputCheckInAndCheckOutDates;
+import static model.Reservation.inputCheckInAndCheckOutDates;
 
 public class Main {
     public static void main(String[] args) {

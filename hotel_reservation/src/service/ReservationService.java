866327d935aeacdfc0c61aabb006929f9c490fac
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-06-12 21:21:40 -0400

    refactor: Refactoring production code
    Replace "return statement" with code that finds rooms available for reservation, puts them into "availableRoomCollection", and  returns "availableRoomCollection" inside "findRooms()" method of "ReservationService.java" java file in "service" package to find and return rooms available for reservation

--- hotel_reservation/src/service/ReservationService.java
+++ hotel_reservation/src/service/ReservationService.java
@@ -114,7 +114,18 @@ public class ReservationService {
         return Reservation.getInstance(customer, room, checkInDate, checkOutDate);
     }
 
-    public static Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate) { return roomCollection; }
+    public static Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate) {
+        // Checks if Room is Already Reserved
+        for (Reservation reservation : reservationCollection) {
+            // Indicates if Room is Available & Adds Room to "availableRoomCollection" if Room IS Available
+            if (checkInDate.before(reservation.getCheckInDate()) && (checkOutDate.before(reservation.getCheckOutDate()))) { // Checks if "checkInDate" in Reservation is BEFORE "checkInDate" & if "checkOutDate" in Reservation is BEFORE "checkOutDate" in "reservationCollection"
+                availableRoomCollection.add(reservation.getRoom());
+            } else if (checkInDate.after(reservation.getCheckInDate()) && checkOutDate.after(reservation.getCheckOutDate())) { // Checks if "checkInDate" in Reservation is AFTER "checkInDate" & if "checkOutDate" in Reservation is AFTER "checkOutDate" in "reservationCollection"
+                availableRoomCollection.add(reservation.getRoom());
+            }
+        }
+        return availableRoomCollection;
+    }
 
     public static Collection<Reservation> getCustomersReservation(Customer customer) {
         // holds ONLY Reservations of "customer"
